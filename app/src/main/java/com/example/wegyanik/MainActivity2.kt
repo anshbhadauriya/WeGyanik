83000dbe7e31e27d6aed61f48a0f2b37a66aa623
Lenovo <2k23.csai2310627@gmail.com>
2025-06-15 15:30:03 +0530

    First Commit

---
+++ app/src/main/java/com/example/wegyanik/MainActivity2.kt
@@ -0,0 +1,23 @@
+package com.example.wegyanik
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        val loginprompt=findViewById<TextView>(R.id.loginprompt)
+        loginprompt.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
